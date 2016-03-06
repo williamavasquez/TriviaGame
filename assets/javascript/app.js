927d59dc5c558d914a047d1277770bcdaaa664f6
williamavasquez <william.a.vasquez@gmail.com>
2016-03-06 15:17:17 -0500

    question and answers show

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,58 @@
 $(document).ready(function(){
 
-	var QuesDisplayed = {
+	 QuestionDisplayed = {
 		question: "How old is your mom",
-		choices:[10,20,30,50],
-    answer: 40,
+		choices:[10,20,30,70],
+    answer: 30,
 	};
 
 // This button will start the game - execute the function
-	$('button').on('click',play);
+	   $('button').on('click',play);
 	
 	// This is the game function, all the game will ocurre inside startGame
 	function startGame(){
     // start the countdown - 30sec
-    stopwatch.reset();
-    // display the questions
-	}
+    countdown.reset();
+  }
+ // display the questions:
+    $('#question').html(QuestionDisplayed.question)
 
+ for (var i = 0; i < QuestionDisplayed.choices.length; i++) {
+      var diego = ".answer"+[i];
+       $('[data-trivia="' + [i] + '"]').html(QuestionDisplayed.choices[i]);
+  };
+  $(".answer").on('click',function(){
+    // convert the answers into data to pull 
+    SelectedAnswer = ($(this).data('trivia'));
+    SelectedAnswer = parseInt(SelectedAnswer);
+    if (QuestionDisplayed.choices[SelectedAnswer]===QuestionDisplayed.answer){
+      console.log("Correct Answer");
+    } else{
+      console.log("wrong answer GERK!")
+    }
+
+  });
 	// when the player begins the game, the start button will disapear 
 	function play() {
 		$('button').hide('<button>');
 		startGame();
 	};
-
 // 30 second Clock
-stopwatch = {
+countdown = {
   time:0,
   reset:function(){
-    stopwatch.time = 30;
+    countdown.time = 30;
     //change the "display" div to "00:30"
     $('#display').html("00:30");
-    stopwatch.start();
+    countdown.start();
     },
   start:function(){
     //Use setInterval to start the count here
-      counter = setInterval(stopwatch.count,1000);
+      counter = setInterval(countdown.count,1000);
     },
   count: function(){
-    stopwatch.time--;
-    var counting= stopwatch.time;
+    countdown.time--;
+    var counting= countdown.time;
     $("#display").html("00:"+counting);
     },
   };
