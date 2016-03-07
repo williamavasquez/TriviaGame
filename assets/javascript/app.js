9c53e226e5d42e7e96fc9b3617d9eda4a18d1725
williamavasquez <william.a.vasquez@gmail.com>
2016-03-06 23:57:31 -0500

    Countdown resets, and random questions appear

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,37 +1,93 @@
 $(document).ready(function(){
 
-	 QuestionDisplayed = {
-		question: "How old is your mom",
-		choices:[10,20,30,70],
+  q1 = {
+    question: "How old is your mom",
+    choices:[10,20,30,70],
     answer: 30,
-	};
-
-// This button will start the game - execute the function
-	   $('button').on('click',play);
-	
-	// This is the game function, all the game will ocurre inside startGame
-	function startGame(){
-    // start the countdown - 30sec
-    countdown.reset();
-  }
- // display the questions:
-    $('#question').html(QuestionDisplayed.question)
-
- for (var i = 0; i < QuestionDisplayed.choices.length; i++) {
-      var diego = ".answer"+[i];
-       $('[data-trivia="' + [i] + '"]').html(QuestionDisplayed.choices[i]);
-  };
-  $(".answer").on('click',function(){
-    // convert the answers into data to pull 
-    SelectedAnswer = ($(this).data('trivia'));
-    SelectedAnswer = parseInt(SelectedAnswer);
-    if (QuestionDisplayed.choices[SelectedAnswer]===QuestionDisplayed.answer){
-      console.log("Correct Answer");
-    } else{
-      console.log("wrong answer GERK!")
+    };
+  q2 = {
+    question: "How old is your mom",
+    choices:[100,200,300,700],
+    answer: 30,
+   };   
+  q3 = {
+    question: "How old is your mom",
+    choices:[30,50,50,60],
+    answer: 30,
+    };
+  q4 = {
+    question: "How old is your mom",
+    choices:["will","will","will","will"],
+    answer: 30,
+    };
+  q5 = {
+    question: "How old is your mom",
+    choices:["Nam","Nam","Nam","Nam",],
+    answer: 30,
+    };
+  q6 = {
+    question: "How old is your mom",
+    choices:["Cora","Cora","Cora","Cora",],
+    answer: 30,
+   };
+  q7 = {
+    question: "How old is your mom",
+    choices:["Pollo","Pollo","Pollo","Pollo",],
+    answer: 30,
+    };
+  q8 = {
+    question: "How old is your mom",
+    choices:[10,20,30,70],
+    answer: 30,
+    };
+  q9 = {
+    question: "How old is your mom",
+    choices:[10,20,30,70],
+    answer: 30,
+   };
+  q10 = {
+    question: "How old is your mom",
+    choices:[10,20,30,70],
+    answer: 30,
     }
 
+// Push all the Questions(object) into one array 
+  TotalQs = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+
+// // select a random Question
+function RandomQuestion(){
+  RandomValue = Math.floor((Math.random()*TotalQs.length))
+  random = TotalQs[RandomValue];
+  TotalQs.splice(RandomValue,1);
+
+    // display the questions:
+    $('#question').html(random.question);
+    // display answers
+     for (var i = 0; i < random.choices.length; i++) {
+      $('[data-trivia="' + [i] + '"]').html(random.choices[i]);
+      };
+    // Pull the data to check if the answer is correct or not
+   $(".answer").on('click',function(){
+      SelectedAnswer = ($(this).data('trivia'));
+      SelectedAnswer = parseInt(SelectedAnswer);
+      
+        if (random.choices[SelectedAnswer]===random.answer){
+        console.log("Correct Answer");
+        } else{
+        console.log("wrong answer GERK!")
+    }
   });
+};
+
+// This button will start the game - execute the function
+     $('button').on('click',play);
+  
+  // This is the game function, all the game will ocurre inside startGame
+  function startGame(){
+    // start the countdown - 30sec
+    countdown.reset();
+    RandomQuestion();
+    };
 	// when the player begins the game, the start button will disapear 
 	function play() {
 		$('button').hide('<button>');
@@ -48,13 +104,19 @@ countdown = {
     },
   start:function(){
     //Use setInterval to start the count here
-      counter = setInterval(countdown.count,1000);
+    counter = setInterval(countdown.count,1000);
     },
   count: function(){
-    countdown.time--;
     var counting= countdown.time;
+    if (countdown.time===0){
+      startGame();
+      clearInterval(counter);
+    } else {
+      countdown.time--;
+    }
     $("#display").html("00:"+counting);
     },
   };
+
 // closing script bracket below
 })
