15660c6008833b3cbf5ce6a5f0ace2d0fcb89a46
williamavasquez <william.a.vasquez@gmail.com>
2016-03-07 18:32:54 -0500

    css implemented

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,55 @@
 $(document).ready(function(){
+  $("#container").hide();
 
   q1 = {
-    question: "How old is your mom",
-    choices:[10,20,30,70],
-    answer: 30,
+    question: "On episode one, 'Days Gone By', Rick was in a gas station trying to look for fuel. He then saw a girl zombie. What did he do with the girl zombie?",
+    choices:[" He filled her mouth with gas until she exploded","He shot her in the head using his gun","Rick used his car keys to stab her in the forehead","Rick tripped her and stomped her head"],
+    answer: "He shot her in the head using his gun",
     };
   q2 = {
-    question: "How old is your mom",
-    choices:[100,200,300,700],
-    answer: 30,
+    question: 'On episode one, "Days Gone By", Rick managed to get out of the hospital. He then encountered two men, a father and son. What were the father and son\'s names?',
+    choices:["James and Larry Benson","Harry and Jack Cox","Morgan and Duane Jones","Michael and Franzen Leitner"],
+    answer: "Morgan and Duane Jones",
    };   
   q3 = {
-    question: "How old is your mom",
-    choices:[30,50,50,60],
-    answer: 30,
+    question: 'On episode one, "Days Gone By", Rick and the "Father and Son" realized they need firepower to be alive. Where did they go to find ammunition and guns?',
+    choices:[" Marty Mulligan Gun Shop","The police station","The guard house","Morgan's cousin, Jim"],
+    answer: "The police station",
     };
   q4 = {
-    question: "How old is your mom",
-    choices:["will","will","will","will"],
-    answer: 30,
+    question: "On episode one, 'Days Gone By', When Rick ran out of gas, he used a horse as an alternative and continued his way to 'the safe zone', Atlanta where he found himself trapped with a hundred walkers. He was able to get inside a safe place. What or where was that safe place?",
+    choices:["Gasoline truck","Coffee shop","Army tank","Security car"],
+    answer: "Army tank",
     };
   q5 = {
-    question: "How old is your mom",
-    choices:["Nam","Nam","Nam","Nam",],
-    answer: 30,
+    question: 'On episode two, "Guts", Rick and Glenn finally made it to the safe zone, the department store. In there, Rick was in a fight with Merle Dixon. Losing his temper, what did he do to Merle?',
+    choices:["Rick used his taser against Merle","Rick used his baton to beat up Merle","He broke Merle's arms and pushed him on the floor","He handcuffed Merle in a metal pipe",],
+    answer: "He handcuffed Merle in a metal pipe",
     };
   q6 = {
-    question: "How old is your mom",
-    choices:["Cora","Cora","Cora","Cora",],
-    answer: 30,
+    question:  'In episode two, "Guts", Glenn and Rick tried to do a rescue. Rick used the box truck while Glenn used another car\s alarm to distract the walkers. What was the car Glenn used?',
+    choices:["red Volkswagen","police cruiser","Dodge Challenger","An F1 racecar",],
+    answer: "Dodge Challenger",
    };
   q7 = {
-    question: "How old is your mom",
-    choices:["Pollo","Pollo","Pollo","Pollo",],
-    answer: 30,
+    question:  'In the third episode, "Tell it to The Frogs", while Rick, Glenn, Daryl, and T-Dog went back to the mart to rescue Merle, a fight broke in the camp. Who triggered the tension?',
+    choices:["Ed","Carl","Lori","Ellen",],
+    answer: "Ed",
     };
   q8 = {
-    question: "How old is your mom",
-    choices:[10,20,30,70],
-    answer: 30,
+    question:  'In the third episode, "Tell it to The Frogs", the rescue team of Rick, Glenn, Daryl, and T-Dog finally reached the rooftop where Merle was handcuffed. They were surprised to see... what on the rooftop?',
+    choices:["Merle who was then a zombie","Merle's corpse surrounded by vultures","an amputated hand on the floor","a dozen of walkers"],
+    answer: "an amputated hand on the floor",
     };
   q9 = {
-    question: "How old is your mom",
-    choices:[10,20,30,70],
-    answer: 30,
+    question:  'In the episode "Vatos", the rescue team decided to go back to the camp but before that, they tried to retrieve Rick\'s bag-full of guns and ammo that he dropped near the tank. A member of the rescue team was kidnapped by a group of men for them to have the guns. Who was the kidnapped rescue team member?',
+    choices:[" Daryl"," T-Dog","Glenn","Rick"],
+    answer: "Glenn",
    };
   q10 = {
-    question: "How old is your mom",
-    choices:[10,20,30,70],
-    answer: 30,
+    question: "Daryl was very useful with his team because of his weapon. What was his 'silent' weapon?",
+    choices:["Spear","Dagger","Crossbow","Taekwondo"],
+    answer: "Crossbow",
     }
 
 // Push all the Questions(object) into one array 
@@ -66,16 +67,21 @@ function RandomQuestion(){
      for (var i = 0; i < random.choices.length; i++) {
       $('[data-trivia="' + [i] + '"]').html(random.choices[i]);
       };
+
     // Pull the data to check if the answer is correct or not
    $(".answer").on('click',function(){
       SelectedAnswer = ($(this).data('trivia'));
       SelectedAnswer = parseInt(SelectedAnswer);
       
         if (random.choices[SelectedAnswer]===random.answer){
-        console.log("Correct Answer");
-        } else{
-        console.log("wrong answer GERK!")
-    }
+          alert("You got it Right!")
+          setInterval(startGame,2000);
+          clearInterval(counter);
+      } else{
+          alert("Wrong Answer, the correct answer was " + random.answer);
+          setInterval(startGame,2000);
+          clearInterval(counter);
+      }
   });
 };
 
@@ -90,8 +96,9 @@ function RandomQuestion(){
     };
 	// when the player begins the game, the start button will disapear 
 	function play() {
-		$('button').hide('<button>');
-		startGame();
+    $('button').hide('<button>');
+    $("#container").show(1000);
+    startGame();
 	};
 // 30 second Clock
 countdown = {
@@ -108,15 +115,25 @@ countdown = {
     },
   count: function(){
     var counting= countdown.time;
-    if (countdown.time===0){
-      startGame();
+    if (TotalQs.length==0){
+      reseter();
+    } else if (countdown.time===0){
       clearInterval(counter);
+      startGame();
     } else {
       countdown.time--;
-    }
+    };
     $("#display").html("00:"+counting);
     },
   };
 
+function reseter(){
+    $("#container").hide();
+    TotalQs = [q1,q2,q3,q4,q5,q6,q7,q8,q9,q10];
+    countdown.time=30;
+    alert("Game is Over")
+    $('button').show(2000); 
+    clearInterval(counter); 
+}
 // closing script bracket below
 })
